c5d8a991e5efe27949805e18dfbd5d0a38681de2
Florian Springer <florian.springer@minnospherelab.onmicrosoft.com>
2021-03-11 17:32:44 +0100

    Fixed mapping problems with API Hub data

--- dialogs/userProfileDialog.js
+++ dialogs/userProfileDialog.js
@@ -4,24 +4,23 @@
 const { CardFactory, AttachmentLayoutTypes } = require('botbuilder');
 
 const {
-  OAuthPrompt,
-  ComponentDialog,
-  DialogSet,
-  DialogTurnStatus,
-  TextPrompt,
-  WaterfallDialog,
+    OAuthPrompt,
+    ComponentDialog,
+    DialogSet,
+    DialogTurnStatus,
+    TextPrompt,
+    WaterfallDialog,
 } = require('botbuilder-dialogs');
 
 const { OAuthHelpers } = require('../helpers/oAuthHelpers');
 const { SimpleSAPGraphClient } = require('../simple-SAP-graph-client');
 const { SimpleSAPAPIHubClient } = require('../simple-SAP-API-Hub-client');
 const { SAPGraphHelper } = require('../helpers/SAP-graph-helper');
-const {
-  AdaptiveCardsHelper,
-} = require('../helpers/adaptiveCards/AdaptiveCardsHelper');
+const { AdaptiveCardsHelper } = require('../helpers/adaptiveCards/AdaptiveCardsHelper');
 
 const salesOrderAdaptiveCard = require('../helpers/adaptiveCards/salesOrderAdaptiveCard.json');
 const AdaptiveCardTemplating = require('adaptivecards-templating');
+const { AddressFaxNumber } = require('@sap/cloud-sdk-vdm-business-partner-service');
 
 const OAUTH_PROMPT = 'OAuthPrompt';
 const USER_PROFILE = 'USER_PROFILE';
@@ -29,214 +28,207 @@ const WATERFALL_DIALOG = 'WATERFALL_DIALOG';
 const TEXT_PROMPT = 'TEXT_PROMPT';
 
 class UserProfileDialog extends ComponentDialog {
-  // The user interaction flow is defined in the constructor
-  constructor(userState) {
-    super('userProfileDialog');
+    // The user interaction flow is defined in the constructor
+    constructor(userState) {
+        super('userProfileDialog');
 
-    this.userProfile = userState.createProperty(USER_PROFILE);
+        this.userProfile = userState.createProperty(USER_PROFILE);
 
-    // Prompt for OAUTH
-    this.addDialog(
-      new OAuthPrompt(OAUTH_PROMPT, {
-        connectionName: process.env.connectionName,
-        text: 'Please Sign In',
-        title: 'Sign In',
-        timeout: 300000,
-      })
-    );
+        // Prompt for OAUTH
+        this.addDialog(
+            new OAuthPrompt(OAUTH_PROMPT, {
+                connectionName: process.env.connectionName,
+                text: 'Please Sign In',
+                title: 'Sign In',
+                timeout: 300000,
+            })
+        );
 
-    this.addDialog(new TextPrompt(TEXT_PROMPT));
+        this.addDialog(new TextPrompt(TEXT_PROMPT));
 
-    // Start the user interaction / waterfall dialog
-    this.addDialog(
-      new WaterfallDialog(WATERFALL_DIALOG, [
-        this.promptStep.bind(this),
-        this.loginStep.bind(this),
-        this.commandStep.bind(this),
-        this.processStep.bind(this),
-        this.sapGraphStep.bind(this),
-      ])
-    );
+        // Start the user interaction / waterfall dialog
+        this.addDialog(
+            new WaterfallDialog(WATERFALL_DIALOG, [
+                this.promptStep.bind(this),
+                this.loginStep.bind(this),
+                this.commandStep.bind(this),
+                this.processStep.bind(this),
+                this.sapGraphStep.bind(this),
+            ])
+        );
 
-    this.initialDialogId = WATERFALL_DIALOG;
-  }
-
-  /**
-   * The run method handles the incoming activity (in the form of a TurnContext) and passes it through the dialog system.
-   * If no dialog is active, it will start the default dialog.
-   * @param {*} turnContext
-   * @param {*} accessor
-   */
-  async run(turnContext, accessor) {
-    const dialogSet = new DialogSet(accessor);
-    dialogSet.add(this);
-
-    const dialogContext = await dialogSet.createContext(turnContext);
-    const results = await dialogContext.continueDialog();
-    if (results.status === DialogTurnStatus.empty) {
-      await dialogContext.beginDialog(this.id);
+        this.initialDialogId = WATERFALL_DIALOG;
     }
-  }
 
-  // Here we start
-  async promptStep(stepContext) {
-    await stepContext.context.sendActivity(
-      'Welcome to the Microsoft Graph & SAP Graph Chatbot Demo.'
-    );
-    return await stepContext.beginDialog(OAUTH_PROMPT);
-  }
+    /**
+     * The run method handles the incoming activity (in the form of a TurnContext) and passes it through the dialog system.
+     * If no dialog is active, it will start the default dialog.
+     * @param {*} turnContext
+     * @param {*} accessor
+     */
+    async run(turnContext, accessor) {
+        const dialogSet = new DialogSet(accessor);
+        dialogSet.add(this);
 
-  async loginStep(stepContext) {
-    // Get the token from the previous step. Note that we could also have gotten the
-    // token directly from the prompt itself. There is an example of this in the next method.
-    const tokenResponse = stepContext.result;
-    if (tokenResponse && tokenResponse.token) {
-      await stepContext.context.sendActivity('You are now logged in.');
-      return await stepContext.prompt(TEXT_PROMPT, {
-        prompt:
-          "Please type 'inbox' to display your Outlook inbox via the Microsoft Graph API or 'me' for your profile",
-      });
+        const dialogContext = await dialogSet.createContext(turnContext);
+        const results = await dialogContext.continueDialog();
+        if (results.status === DialogTurnStatus.empty) {
+            await dialogContext.beginDialog(this.id);
+        }
     }
-    await stepContext.context.sendActivity(
-      'Login was not successful please try again.'
-    );
-    return await stepContext.endDialog();
-  }
 
-  async commandStep(step) {
-    step.values.command = step.result;
+    // Here we start
+    async promptStep(stepContext) {
+        await stepContext.context.sendActivity(
+            'Welcome to the Microsoft Graph & SAP Graph Chatbot Demo.'
+        );
+        return await stepContext.beginDialog(OAUTH_PROMPT);
+    }
 
-    // Call the prompt again because we need the token. The reasons for this are:
-    // 1. If the user is already logged in we do not need to store the token locally in the bot and worry
-    // about refreshing it. We can always just call the prompt again to get the token.
-    // 2. We never know how long it will take a user to respond. By the time the
-    // user responds the token may have expired. The user would then be prompted to login again.
-    //
-    // There is no reason to store the token locally in the bot because we can always just call
-    // the OAuth prompt to get the token or get a new token if needed.
-    return await step.beginDialog(OAUTH_PROMPT);
-  }
+    async loginStep(stepContext) {
+        // Get the token from the previous step. Note that we could also have gotten the
+        // token directly from the prompt itself. There is an example of this in the next method.
+        const tokenResponse = stepContext.result;
+        if (tokenResponse && tokenResponse.token) {
+            await stepContext.context.sendActivity('You are now logged in.');
+            return await stepContext.prompt(TEXT_PROMPT, {
+                prompt:
+                    "Please type 'inbox' to display your Outlook inbox via the Microsoft Graph API or 'me' for your profile",
+            });
+        }
+        await stepContext.context.sendActivity(
+            'Login was not successful please try again.'
+        );
+        return await stepContext.endDialog();
+    }
 
-  async processStep(step) {
-    if (step.result) {
-      // We do not need to store the token in the bot. When we need the token we can
-      // send another prompt. If the token is valid the user will not need to log back in.
-      // The token will be available in the Result property of the task.
-      const tokenResponse = step.result;
+    async commandStep(step) {
+        step.values.command = step.result;
 
-      // If we have the token use the user is authenticated so we may use it to make API calls.
-      if (tokenResponse && tokenResponse.token) {
-        const parts = (step.values.command || '').toLowerCase().split(' ');
+        // Call the prompt again because we need the token. The reasons for this are:
+        // 1. If the user is already logged in we do not need to store the token locally in the bot and worry
+        // about refreshing it. We can always just call the prompt again to get the token.
+        // 2. We never know how long it will take a user to respond. By the time the
+        // user responds the token may have expired. The user would then be prompted to login again.
+        //
+        // There is no reason to store the token locally in the bot because we can always just call
+        // the OAuth prompt to get the token or get a new token if needed.
+        return await step.beginDialog(OAUTH_PROMPT);
+    }
 
-        const command = parts[0];
+    async processStep(step) {
+        if (step.result) {
+            // We do not need to store the token in the bot. When we need the token we can
+            // send another prompt. If the token is valid the user will not need to log back in.
+            // The token will be available in the Result property of the task.
+            const tokenResponse = step.result;
 
-        switch (command) {
-          case 'me':
-            await OAuthHelpers.listMe(step.context, tokenResponse);
-            break;
+            // If we have the token use the user is authenticated so we may use it to make API calls.
+            if (tokenResponse && tokenResponse.token) {
+                const parts = (step.values.command || '').toLowerCase().split(' ');
 
-          // This case is relevant for the hands-on lab. Search in the inbox via MS Graph
-          case 'inbox':
-            await OAuthHelpers.listRecentMail(step.context, tokenResponse);
-            break;
-          default:
+                const command = parts[0];
+
+                switch (command) {
+                    case 'me':
+                        await OAuthHelpers.listMe(step.context, tokenResponse);
+                        break;
+
+                    // This case is relevant for the hands-on lab. Search in the inbox via MS Graph
+                    case 'inbox':
+                        await OAuthHelpers.listRecentMail(step.context, tokenResponse);
+                        break;
+                    default:
+                        await step.context.sendActivity(
+                            `Your token is ${tokenResponse.token}`
+                        );
+                }
+                return await step.prompt(TEXT_PROMPT, {
+                    prompt:
+                        'Please type the order number for displaying the sales order details',
+                });
+            }
+        } else {
             await step.context.sendActivity(
-              `Your token is ${tokenResponse.token}`
+                "We couldn't log you in. Please try again later."
             );
         }
-        return await step.prompt(TEXT_PROMPT, {
-          prompt:
-            'Please type the order number for displaying the sales order details',
-        });
-      }
-    } else {
-      await step.context.sendActivity(
-        "We couldn't log you in. Please try again later."
-      );
+
+        return await step.endDialog();
     }
 
-    return await step.endDialog();
-  }
+    // Walters Get the data from the SAP Graph
+    async sapGraphStep(step) {
+        try {
+            const parts = (step.result || '').split(' ');
 
-  // Walters Get the data from the SAP Graph
-  async sapGraphStep(step) {
-    const parts = (step.result || '').split(' ');
+            const simpleSAPClient = process.env.UseSAPGraph.toLowerCase() === 'true'
+                ? new SimpleSAPGraphClient()
+                : new SimpleSAPAPIHubClient();
 
-    var simpleSAPGraphClient = new SimpleSAPGraphClient();
-    // todo let simpleSAPClient = new SimpleSAPAPIHubClient();
+            //Example Graph -> 10, API Hub -> 95644
+            const salesOrders = await simpleSAPClient.getOrderByOrderNumber(parts[0]);
+            const customer = await simpleSAPClient.getCustomerBySalesOrder(
+                salesOrders[0]
+            ); // for demo purposes only first sales order is considered
 
-    const salesOrders = await simpleSAPGraphClient.getOrderByOrderNumber(
-      parts[0]
-    );
-    const customer = await simpleSAPGraphClient.getCustomerBySalesOrder(
-      salesOrders[0]
-    ); // for demo purposes only first sales order is considered
+            // TODO: error handling if no salesOrders were found
+            const numberOfSalesOrders = salesOrders.length;
 
-    // TODO: error handling if no salesOrders were found
-    const numberOfSalesOrders = salesOrders.length;
+            // Create a hero card and loop over graph result set
+            const reply = {
+                attachments: [],
+                attachmentLayout: AttachmentLayoutTypes.List,
+            };
+            for (let cnt = 0; cnt < numberOfSalesOrders; cnt++) {
+                const salesOrder = salesOrders[cnt];
 
-    // Create a hero card and loop over graph result set
-    const reply = {
-      attachments: [],
-      attachmentLayout: AttachmentLayoutTypes.List,
-    };
-    for (let cnt = 0; cnt < numberOfSalesOrders; cnt++) {
-      const salesOrder = salesOrders[cnt];
+                // get Sales Order Adaptive Card JSON Template and adjust it according to the items we have in Sales Order
+                salesOrderAdaptiveCard.body[2].columns = AdaptiveCardsHelper.getItemsTable(
+                    salesOrder.items
+                );
 
-      // get Sales Order Adaptive Card JSON Template and adjust it according to the items we have in Sales Order
-      salesOrderAdaptiveCard.body[2].columns = AdaptiveCardsHelper.getItemsTable(
-        salesOrder.items
-      );
+                // decide which color to use depending of the Sales Order status
+                const statusColor = SAPGraphHelper.getColorByStatusCode(
+                    salesOrder.processingStatus.code
+                );
 
-      // decide which color to use depending of the Sales Order status
-      const statusColor = SAPGraphHelper.getColorByStatusCode(
-        salesOrder.processingStatus.code
-      );
-      // Todo const statusColor = SAPGraphHelper.getColorByStatusCode(salesOrder.overallSdProcessStatus);
+                const template = new AdaptiveCardTemplating.Template(
+                    salesOrderAdaptiveCard
+                );
 
-      const template = new AdaptiveCardTemplating.Template(
-        salesOrderAdaptiveCard
-      );
-      console.log('Customer phone Numbers: ');
-      console.log(customer.addressData.phoneNumbers);
-      const card = template.expand({
-        $root: {
-          salesOrderID: salesOrder.displayId,
-          customerID: customer.displayId, // salesOrder.customer.id, // TODO: lesen vom BP und anzeigen der displayId
-          distributionChannel: salesOrder.distributionChannel.name,
-          division: salesOrder.division.name,
-          orderDate: salesOrder.orderDate,
-          netAmount: salesOrder.netAmount,
-          currency: salesOrder.currency.code,
-          statusText: salesOrder.processingStatus.name,
-          statusColor: statusColor,
-          contactName: customer.organization.nameDetails.formattedOrgNameLine1, // TODO: find a customer <> Organization
-          contactPhoneNumber: customer.addressData[0].phoneNumbers[0].number,
-          // TODO: telefonnummer
-        },
-      });
+                console.log('Customer phone Numbers: ');
+    
+                console.log(customer.addressData[0].phoneNumbers);
+                const card = template.expand({
+                    $root: {
+                        salesOrderID: salesOrder.displayId,
+                        customerID: customer.displayId, 
+                        distributionChannel: salesOrder.distributionChannel.name,
+                        division: salesOrder.division.name,
+                        orderDate: salesOrder.orderDate,
+                        netAmount: salesOrder.netAmount,
+                        currency: salesOrder.currency.code,
+                        statusText: salesOrder.processingStatus.name,
+                        statusColor: statusColor,
+                        contactName: customer.organization.nameDetails.formattedOrgNameLine1, // TODO: find a customer <> Organization
+                        contactPhoneNumber: customer.addressData[0].phoneNumbers[0].number,
+                    },
+                });
 
-      //todo api hub
-      // const card = template.expand({
-      //     $root: {
-      //       salesOrderID: salesOrder.salesOrder,
-      //       customerID: salesOrder.soldToParty,
-      //       orderDate: salesOrder.salesOrderDate,
-      //       netAmount: salesOrder.totalNetAmount,
-      //       currency: salesOrder.transactionCurrency,
-      //       statusText: salesOrder.overallSDProcessStatus,
-      //       statusColor: statusColor,
-      //     },
-      //   });
+                const cardInBotFormat = CardFactory.adaptiveCard(card);
 
-      const cardInBotFormat = CardFactory.adaptiveCard(card);
+                reply.attachments.push(cardInBotFormat);
+            }
+            await step.context.sendActivity(reply);
 
-      reply.attachments.push(cardInBotFormat);
+        } catch (error) {
+            console.log(error);
+            await step.context.sendActivity('Accessing data from SAP was not possible.');
+        }
+
+        return await step.endDialog();
     }
-    await step.context.sendActivity(reply);
-
-    return await step.endDialog();
-  }
 }
 
 module.exports.UserProfileDialog = UserProfileDialog;
--- simple-SAP-API-Hub-client.js
+++ simple-SAP-API-Hub-client.js
@@ -3,6 +3,7 @@
 
 let {
   BusinessPartner,
+  BpContactToAddress
 } = require('@sap/cloud-sdk-vdm-business-partner-service');
 let {
   SalesOrder,
@@ -13,33 +14,92 @@ let {
  * Wrapperclass for the SAP API Hub.
  */
 class SimpleSAPAPIHubClient {
-  async getCustomersByLastName(customerLastName) {
-    return BusinessPartner.requestBuilder()
+  async getCustomerBySalesOrder(salesOrder) {
+    let businessPartners = await BusinessPartner.requestBuilder()
       .getAll()
       .select(
         BusinessPartner.BUSINESS_PARTNER,
-        BusinessPartner.BUSINESS_PARTNER_NAME
+        BusinessPartner.BUSINESS_PARTNER_NAME,
+        BusinessPartner.ORGANIZATION_BP_NAME_1,
+        BusinessPartner.TO_BUSINESS_PARTNER_ADDRESS
       )
-      .filter(BusinessPartner.BUSINESS_PARTNER_NAME.equals(customerLastName))
-      .top(5)
+      .filter(BusinessPartner.BUSINESS_PARTNER.equals(salesOrder.customer.id))
+      .top(1)
       .addCustomHeaders({ APIKey: process.env.SAPAPIHubKey })
       .execute({
         url: process.env.SAPAPIHubDestination,
       });
+      
+    return this.mapToSAPGraphCustomer(businessPartners[0]);
   }
 
-  async getSalesOrderForCustomerId(customerId) {
-    return SalesOrder.requestBuilder()
-      .getAll()
-      .select(SalesOrder.ALL_FIELDS, SalesOrder.TO_ITEM)
-      .filter(SalesOrder.SOLD_TO_PARTY.equals(customerId))
-      .top(5)
-      .addCustomHeaders({ APIKey: process.env.SAPAPIHubKey })
-      .execute({
-        url: process.env.SAPAPIHubDestination,
-      });
+  async getOrderByOrderNumber(orderNumber) {
+    let salesOrders = await SalesOrder.requestBuilder()
+    .getAll()
+    .select(SalesOrder.ALL_FIELDS, SalesOrder.TO_ITEM)
+    .filter(SalesOrder.SALES_ORDER.equals(orderNumber))
+    .top(5)
+    .addCustomHeaders({ APIKey: process.env.SAPAPIHubKey })
+    .execute({
+      url: process.env.SAPAPIHubDestination,
+    });
+
+    return [this.mapToSAPGraphSalesOrder(salesOrders[0])];
   }
-  //.addCustomQueryParameters({'expand':'to_Item'})
+
+  mapToSAPGraphSalesOrder(salesOrder){
+    //ToDo read process status text
+    let graphSalesOrder = {
+        "displayId":salesOrder.salesOrder,
+        "customer":{
+            "id": salesOrder.soldToParty
+        },
+        "distributionChannel":{
+            "name":salesOrder.distributionChannel
+        },
+        "division":{
+            "name":salesOrder.organizationDivision
+        },
+        "orderDate": salesOrder.salesOrderDate._d.toISOString().substr(0,19) + 'Z',
+        "netAmount":salesOrder.totalNetAmount,
+        "currency":{
+            "code":salesOrder.transactionCurrency
+        },
+        "processingStatus":{
+            "code": salesOrder.overallSdProcessStatus,
+            "name":"Open"
+        },
+        "items":[]
+    }
+
+    salesOrder.toItem.forEach(element => {
+        graphSalesOrder.items.push({"text": element.salesOrderItemText,"netAmount": element.netAmount.c[0], "quantity": element.requestedQuantity.c[0]});
+    });
+
+    return graphSalesOrder;
+  }
+
+  mapToSAPGraphCustomer(businessPartner){
+    //ToDo select phoneNumber
+    //BpContactToAddress.TO_PHONE_NUMBER
+    let phone = '112233';
+    
+    let graphCustomer = {
+        "displayId":businessPartner.businessPartner,
+        "addressData":[{
+            "phoneNumbers":[
+                {"number":phone}
+            ]
+        }],
+        "organization":{
+            "nameDetails":{
+                "formattedOrgNameLine1":businessPartner.organizationBpName1
+            }
+        }
+    }
+    return graphCustomer;
+  }
+
 }
 
 exports.SimpleSAPAPIHubClient = SimpleSAPAPIHubClient;
